074fde67e54262d7b27c704b7310cf5290d8b6b0
Kari Bancroft <kariabancroft@gmail.com>
2018-03-07 13:43:52 -0800

    remove status codes per charles comment

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -76,13 +76,12 @@ class WorksController < ApplicationController
       else
         flash[:result_text] = "Could not upvote"
         flash[:messages] = vote.errors.messages
-        status = 302
       end
     else
       flash[:result_text] = "You must log in to do that"
       # There is a long web standards debate about returning contents with a 401 status code
-      # but you cannot auto-redirect without a 30x status code, hence using 302 here (and above).
-      status = 302
+      # but you cannot auto-redirect without a 30x status code, hence using the default redirect here
+      # (and above) via no special status code.
     end
 
     # Refresh the page to show either the updated vote count
