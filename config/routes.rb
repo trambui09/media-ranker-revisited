b5d2170a32eebb0af222b437e07d6eb561e7a27d
dee <dee@adadevelopersacademy.org>
2019-04-28 17:22:11 -0700

    conform practices to C11 examples: sessionscontroller -> userscontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "works#root"
-  get "/login", to: "sessions#login_form", as: "login"
-  post "/login", to: "sessions#login"
-  post "/logout", to: "sessions#logout", as: "logout"
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
 
   resources :works
   post "/works/:id/upvote", to: "works#upvote", as: "upvote"
