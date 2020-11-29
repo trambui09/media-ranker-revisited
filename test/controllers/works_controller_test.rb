70f2811208b9d8a32c2c0dce12aec70e2332b4ab
Tram Bui <pbui17@cmc.edu>
2020-11-29 11:22:06 -0600

    added tests for works show and index for logged in user and not

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -34,13 +34,24 @@ describe WorksController do
   INVALID_CATEGORIES = ["nope", "42", "", "  ", "albumstrailingtext"]
 
   describe "index" do
-    it "succeeds when there are works" do
+    it "succeeds when there are works and user is logged in" do
+      perform_login
       get works_path
 
       must_respond_with :success
     end
 
+    it "fails when user is not logged in" do
+      get works_path
+
+      expect(flash[:result_text]).must_equal "You must log in to do that"
+      must_redirect_to root_path
+
+    end
+
     it "succeeds when there are no works" do
+      perform_login
+
       Work.all do |work|
         work.destroy
       end
@@ -96,10 +107,19 @@ describe WorksController do
   end
 
   describe "show" do
-    it "succeeds for an extant work ID" do
+    it "succeeds for an extant work ID and user is logged in" do
+      perform_login
       get work_path(existing_work.id)
 
       must_respond_with :success
+    end
+
+    it "fails when user is not logged in" do
+      get work_path(existing_work.id)
+
+      expect(flash[:result_text]).must_equal "You must log in to do that"
+      must_redirect_to root_path
+
     end
 
     it "renders 404 not_found for a bogus work ID" do
